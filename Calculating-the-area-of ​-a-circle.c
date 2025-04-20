6491265146314aa382438e46f1ca1a4cb59d5e7f
Younes AMAZZAL <younesamazzalwork@gmail.com>
2025-04-20 20:26:49 +0100

    Add files via upload

---
+++ Calculating-the-area-of ​-a-circle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    float r;
+    const float pi = 3.14;
+
+    printf("Beam = ");
+    scanf("%f",&r);
+
+    printf("the area of the circle is : %0.2f",pi*pow(r,2));4
+
+    // PI * R^2
+
+    return 0;
+}
+// 20/avril/2025 19:23
